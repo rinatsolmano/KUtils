13b9cfe9f9e0077c2952ec034fc6811ecfb5675f
Rinat Suleimanov <coderinx@gmail.com>
2019-07-17 22:25:37 -0400

    [#1] created time measurement functions

---
+++ src/main/kotlin/org/coderinx/utils/time/Measurements.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2019. coderinx.org
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+@file:kotlin.jvm.JvmName("MeasurementsKt")
+
+package org.coderinx.utils.time
+
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeUnit.*
+
+data class MeasuredResult<T>(
+    val time: Long,
+    val result: T
+)
+
+/**
+ * Executes the given [block] and returns it result and elapsed time in given [timeUnit].
+ */
+@JvmOverloads
+fun <T> withTimeMeasurement(timeUnit: TimeUnit = NANOSECONDS, block: () -> T): MeasuredResult<T> {
+    val start = System.nanoTime()
+    val result = block.invoke()
+    val elapsed = System.nanoTime() - start
+    return when (timeUnit) {
+        NANOSECONDS -> MeasuredResult(elapsed, result)
+        MICROSECONDS -> MeasuredResult(NANOSECONDS.toMicros(elapsed), result)
+        MILLISECONDS -> MeasuredResult(NANOSECONDS.toMicros(elapsed), result)
+        SECONDS -> MeasuredResult(NANOSECONDS.toSeconds(elapsed), result)
+        MINUTES -> MeasuredResult(NANOSECONDS.toMinutes(elapsed), result)
+        HOURS -> MeasuredResult(NANOSECONDS.toHours(elapsed), result)
+        DAYS -> MeasuredResult(NANOSECONDS.toDays(elapsed), result)
+    }
+}
+
+/**
+ * Executes the given [block] and returns elapsed time in given [timeUnit].
+ */
+@JvmOverloads
+fun measureTime(timeUnit: TimeUnit = NANOSECONDS, block: () -> Unit): Long {
+    val start = System.nanoTime()
+    block.invoke()
+    val elapsed = System.nanoTime() - start
+    return when (timeUnit) {
+        NANOSECONDS -> (elapsed)
+        MICROSECONDS -> (NANOSECONDS.toMicros(elapsed))
+        MILLISECONDS -> (NANOSECONDS.toMicros(elapsed))
+        SECONDS -> (NANOSECONDS.toSeconds(elapsed))
+        MINUTES -> (NANOSECONDS.toMinutes(elapsed))
+        HOURS -> (NANOSECONDS.toHours(elapsed))
+        DAYS -> (NANOSECONDS.toDays(elapsed))
+    }
+}
+
