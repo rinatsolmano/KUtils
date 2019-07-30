05fb4485f4ac24fbc61ee313c3518310c579defb
Rinat Suleimanov <coderinx@gmail.com>
2019-07-29 21:51:22 -0400

    [#14] fixed milliseconds conversion, new default

--- src/main/kotlin/org/coderinx/utils/time/Measurements.kt
+++ src/main/kotlin/org/coderinx/utils/time/Measurements.kt
@@ -19,14 +19,14 @@ data class MeasuredResult<T>(
  * Executes the given [block] and returns it result and elapsed time in given [timeUnit].
  */
 @JvmOverloads
-fun <T> withTimeMeasurement(timeUnit: TimeUnit = NANOSECONDS, block: () -> T): MeasuredResult<T> {
+fun <T> withTimeMeasurement(timeUnit: TimeUnit = MILLISECONDS, block: () -> T): MeasuredResult<T> {
     val start = System.nanoTime()
     val result = block.invoke()
     val elapsed = System.nanoTime() - start
     return when (timeUnit) {
         NANOSECONDS -> MeasuredResult(elapsed, result)
         MICROSECONDS -> MeasuredResult(NANOSECONDS.toMicros(elapsed), result)
-        MILLISECONDS -> MeasuredResult(NANOSECONDS.toMicros(elapsed), result)
+        MILLISECONDS -> MeasuredResult(NANOSECONDS.toMillis(elapsed), result)
         SECONDS -> MeasuredResult(NANOSECONDS.toSeconds(elapsed), result)
         MINUTES -> MeasuredResult(NANOSECONDS.toMinutes(elapsed), result)
         HOURS -> MeasuredResult(NANOSECONDS.toHours(elapsed), result)
@@ -38,14 +38,14 @@ fun <T> withTimeMeasurement(timeUnit: TimeUnit = NANOSECONDS, block: () -> T): M
  * Executes the given [block] and returns elapsed time in given [timeUnit].
  */
 @JvmOverloads
-fun measureTime(timeUnit: TimeUnit = NANOSECONDS, block: () -> Unit): Long {
+fun measureTime(timeUnit: TimeUnit = MILLISECONDS, block: () -> Unit): Long {
     val start = System.nanoTime()
     block.invoke()
     val elapsed = System.nanoTime() - start
     return when (timeUnit) {
         NANOSECONDS -> (elapsed)
         MICROSECONDS -> (NANOSECONDS.toMicros(elapsed))
-        MILLISECONDS -> (NANOSECONDS.toMicros(elapsed))
+        MILLISECONDS -> (NANOSECONDS.toMillis(elapsed))
         SECONDS -> (NANOSECONDS.toSeconds(elapsed))
         MINUTES -> (NANOSECONDS.toMinutes(elapsed))
         HOURS -> (NANOSECONDS.toHours(elapsed))
